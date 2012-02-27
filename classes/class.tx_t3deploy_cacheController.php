5c7419867cb9112e344eca44849357b12776bfb6
Sebastiaan van Parijs <svparijs@tryweb.nl>
2012-02-27 16:46:38 +0100

    T3Deploy cleanup

--- classes/class.tx_t3deploy_cacheController.php
+++ classes/class.tx_t3deploy_cacheController.php
@@ -31,66 +31,38 @@ class tx_t3deploy_cacheController {
 				
 		$this->setTCE();
         // this seems to initalized a BE-User
-        $this->TCE->start(Array(),Array());
-        // so this line does not throw an error any more
-        #$TCE->clear_cache('tt_news',$oneRecordId);
-	
-		
-		/*function clearpagecache($pid_list) {
-$tce = t3lib_div::makeInstance('t3lib_TCEmain');
-$pid_array = explode(',', $pid_list);
-foreach ($pid_array as $pid) {
-$tce->clear_cacheCmd($pid);
-}
-}
-		 * 
-		 * <?php 
-
-define("PATH_typo3conf", dirname(dirname(dirname(__FILE__)))."/"); 
-define("PATH_site", dirname(PATH_typo3conf)."/"); 
-define("PATH_typo3", PATH_site."typo3/");       // Typo-configuraton path 
-define("PATH_t3lib", PATH_site."t3lib/"); 
-define('TYPO3_MODE','BE'); 
-ini_set('error_reporting', E_ALL ^ E_NOTICE); 
-
-require_once (PATH_t3lib.'class.t3lib_div.php'); 
-require_once (PATH_t3lib.'class.t3lib_extmgm.php'); 
-√require_once (PATH_t3lib.'class.t3lib_tcemain.php'); 
-
-require_once(PATH_t3lib.'config_default.php'); 
-
-if (!defined ("TYPO3_db")) die ("The configuration file was not included."); 
-require_once(PATH_t3lib.'class.t3lib_db.php');          // The database library 
-$TYPO3_DB = t3lib_div::makeInstance('t3lib_db'); 
-$TYPO3_DB->sql_pconnect (TYPO3_db_host, TYPO3_db_username, TYPO3_db_password); 
-$TYPO3_DB->sql_select_db (TYPO3_db); 
-
-√$tce = t3lib_div::makeInstance('t3lib_TCEmain'); 
-$tce->clear_cacheCmd(40);  // ID of the page for which to clear the cache 
-
-?> 
-		 */
-	
+        $this->TCE->start(Array(),Array());	
 	}
 	
 	/**
 	 * Updates the database structure.
 	 *
 	 * @param array $arguments Optional arguemtns passed to this action
-	 * @return string
+	 * @return void
 	 */
 	public function clearcacheAction(){
 		
+		// The option all clears all Tables, IF the user has the TSconfig settings or is admin. We don't want a CLI user with Admin priveledges!!!!
 		$this->TCE->clear_cacheCmd('all');
 		
-		#if()
+		// The BE user (_cli_t3deploy) needs to be ADMIN to run this command via clear_cacheCmd, therefor we run the removeCacheFiles directly
 		$this->TCE->removeCacheFiles();
 	}
 	
+	/**
+	 * Sets the TCEmain object
+	 * 
+	 * #return void
+	 */
 	private function setTCE(){
 		$this->TCE = t3lib_div::makeInstance('t3lib_TCEmain');
 	}
 	
+	/**
+	 *	Gets the TCEmain object
+	 * 
+	 * @return t3lib_TCEmain $TCE
+	 */
 	public function getTCE(){
 		return $this->TCE;
 	}
